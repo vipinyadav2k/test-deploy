05ed0738c39db119a8f3c8ddf6400cdd97ac91ef
vipinyadav2k <vipinyadav2k@gmail.com>
2025-02-26 13:43:23 +0530

    deploying-2

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    async redirects() {
+        return [
+          {
+            source: "https://www.test-deploy-xi-eight.vercel.app/:path*",
+            destination: "https://test-deploy-xi-eight.vercel.app/:path*",
+            permanent: true,
+          },
+        ];
+      },
+};
 
 export default nextConfig;
